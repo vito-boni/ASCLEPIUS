37eec67a8458876aead3f951b860ba378c5d59f0
Vito B. <boni.vito@hotmail.com>
2025-05-09 08:31:23 +0700

    Replace all contents

--- src/scripts/api.js
+++ src/scripts/api.js
@@ -1,5 +1,5 @@
 // TODO: Silakan sesuaikan BASE URL dari endpoint Anda
-const BASE_URL = 'YOUR_BASE_URL';
+const BASE_URL = 'http://35.219.43.192:8080';
 
 const ENDPOINT = {
   predict: `${BASE_URL}/predict`,
